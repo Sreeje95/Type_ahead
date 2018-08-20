98bdb2e320d6e9fe9fdc7edf19bcd8a97bfae8d0
Sreeje95 <42197812+Sreeje95@users.noreply.github.com>
2018-08-20 09:55:08 +0530

    Add files via upload

---
+++ Input_trainer.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 13 10:22:11 2018
+
+@author: Sreeje.a
+"""
+
+import re
+import os , sys
+sys.path.insert(0,"C:\Users\Sreeje.a\Desktop\Type-ahead_GIT")
+import Config as cfg
+import pandas as pd
+import operator
+
+def input_prep(ip_path):
+    
+    extension = os.path.splitext(ip_path)[1]
+    
+    if extension==".txt":
+        tokens=[]
+        with open(cfg.INPUT_PATH) as f:      
+          for line in f:
+            if not line.isspace():
+                line=line.lower()
+                line_new = re.sub('[^a-zA-Z0-9\n\.]', ' ', line)
+                line_new = line_new.replace(',', '').replace('.','')
+            items=line_new.split()
+            tokens+=items   
+            
+    elif extension==".csv":
+        data=pd.read_csv(cfg.INPUT_PATH)
+        column_names=data.columns.tolist()
+        tok=[]
+        for i in column_names:
+            col=data[i].tolist()
+            for line in col:
+                if str(line)!="nan":
+        
+                    line=line.lower()
+                    line_new = re.sub('[^a-zA-Z0-9\n\.]', ' ', line)              
+                    line_new = line_new.replace(',', '').replace('.','')
+                    tok.append(line_new.split())
+                
+        tokens=reduce(operator.concat,tok)        
+                
+#        print tokens 
+        
+    return tokens
+            
+        
+                     
+            
+            
+            
+            
+            
+            
+            
